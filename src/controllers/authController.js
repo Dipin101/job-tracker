29833892b23131103565615bfa905b779c9d7998
Dipin101 <khatridipin10@gmail.com>
2026-03-17 12:04:31 -0400

    refresh and logout routes complete

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -2,6 +2,16 @@ const pool = require("../db/db");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
+//Generating token for 1h
+const generateAccessToken = (userId) => {
+  return jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: "1h" });
+};
+
+//generate refreshToken for 7d -> industry standard
+const generateRefreshToken = (userId) => {
+  return jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: "7d" });
+};
+
 //Registering
 const register = async (req, res) => {
   const { email, password } = req.body;
@@ -25,11 +35,7 @@ const register = async (req, res) => {
     );
     const user = result.rows[0];
 
-    //generate token
-    const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, {
-      expiresIn: "5h",
-    });
-    res.status(201).json({ token, user });
+    res.status(201).json({ message: "Registered successfully. Please Login" });
   } catch (err) {
     console.error("Resigter Error: ", err);
     res.status(500).json({ message: "Server error" });
@@ -56,14 +62,78 @@ const login = async (req, res) => {
     }
 
     //generate tokens for user
-    const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, {
-      expiresIn: "5h",
+    const accessToken = generateAccessToken(user.id);
+    const refreshToken = generateRefreshToken(user.id);
+
+    //save refresh token to DB
+    const expiresAt = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000); //-> 7 days 7days + 24 hour* 60minute * 60seconds * 1000ms
+    await pool.query(
+      "INSERT INTO refresh_tokens (user_id, token, expires_at) VALUES ($1,$2, $3)",
+      [user.id, refreshToken, expiresAt],
+    );
+
+    //send refresh token at httpOnly cookie
+    res.cookie("refreshToken", refreshToken, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "strict",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
     });
-    res.json({ token, user: { id: user.id, email: user.email } });
+
+    res.json({ accessToken, user: { id: user.id, email: user.email } });
   } catch (err) {
     console.error("Login Error", err);
     res.status(500).json({ message: "Could not Login" });
   }
 };
 
-module.exports = { register, login };
+//Refresh access token
+const refresh = async (req, res) => {
+  const token = req.cookies.refreshToken;
+  if (!token) {
+    return res.status(401).json({ message: "No refresh token" });
+  }
+  try {
+    //check token exists in db or not and not expired
+    const result = await pool.query(
+      "SELECT * FROM refresh_tokens WHERE token = $1 AND expires_at > NOW()",
+      [token],
+    );
+    if (result.rows.length === 0) {
+      return res
+        .status(401)
+        .json({ message: "Invalid or expired refresh token" });
+    }
+
+    // verify JWT signature
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    //issue new access Token
+    const accessToken = generateAccessToken(decoded.userId);
+    res.json({ accessToken });
+  } catch (err) {
+    console.error("Refresh Error", err);
+    res.status(401).json({ message: "Invalidd refresh token" });
+  }
+};
+
+// for logout
+const logout = async (req, res) => {
+  const token = req.cookies.refreshToken;
+  if (!token) {
+    return res.status(401).json({ message: "No refresh token" });
+  }
+  try {
+    //delete from db
+    await pool.query("DELETE FROM refresh_tokens WHERE token =$1", [token]);
+
+    // clear cookies
+    res.clearCookie("refreshToken");
+    res.json({ message: "Logged out successfully" });
+  } catch (err) {
+    console.error("Logout Error", err);
+    res.status(500).json({ message: "Could not logout" });
+  }
+};
+
+module.exports = { register, login, refresh, logout };
--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 require("dotenv").config();
 const express = require("express");
+const cookieParser = require("cookie-parser");
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(cookieParser());
 
 const authRoutes = require("./routes/authRoutes");
 app.use("/api/auth", authRoutes);
