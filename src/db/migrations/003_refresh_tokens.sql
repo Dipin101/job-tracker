910486e9ecab2fe34b6584b655ea85cd7441fc91
Dipin101 <khatridipin10@gmail.com>
2026-03-17 11:29:56 -0400

    added refresh_token db to migrations

---
+++ src/db/migrations/003_refresh_tokens.sql
@@ -0,0 +1,8 @@
+--table for json refresh tokens
+CREATE TABLE IF NOT EXISTS refresh_tokens(
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+  user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+  token VARCHAR(255) UNIQUE NOT NULL,
+  expires_at TIMESTAMP NOT NULL,
+  created_at TIMESTAMP DEFAULT NOW()
+)
